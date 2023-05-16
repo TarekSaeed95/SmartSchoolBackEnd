8fde561debd54ccb154c2fc157d08da59798c758
tarek95 <tare2sa3eed@gmail.com>
2023-05-16 12:08:47 +0300

    tarek initial commit

---
+++ SmartSchool.DAL/Entities/TeacherAttendance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartSchool.DAL.Entities;
+
+public class TeacherAttendance
+{
+    [Key]
+    public int id { set; get; }
+
+    [DataType(DataType.Date)]
+    public DateTime date { set; get; }
+    public bool state { set; get; }
+
+    [ForeignKey("student")]
+    public string TeacherId { set; get; }
+
+    public virtual Teacher? student { get; set; }
+
+}
+
+
